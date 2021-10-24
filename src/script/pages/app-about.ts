a2620b2bc2d22f9cb7586e22019712f1cdfa1a9c
Dpessheva <dpessheva@gmail.com>
2021-10-24 15:14:38 +0200

    update font-family styles

--- src/script/pages/app-about.ts
+++ src/script/pages/app-about.ts
@@ -15,6 +15,18 @@ export class AppAbout extends LitElement {
     return html`
       <div>
         <h2>About Page</h2>
+        <section>
+        <h3>Lorem</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce aliquam leo nec sem consectetur, vel aliquet massa condimentum. Etiam at dapibus orci. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quam metus, lobortis sed volutpat id, elementum sed nibh. Nullam lacinia augue et pretium congue. Ut eleifend hendrerit ultricies. Ut hendrerit tellus porttitor purus convallis fermentum. Aenean cursus lacus libero, non maximus odio mattis quis. In a lorem tincidunt, lobortis ipsum sed, mattis sem. Morbi sapien dolor, sollicitudin eget libero ut, feugiat auctor tortor.</p>
+        </section>
+        <section>
+        <h3>Lorem</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce aliquam leo nec sem consectetur, vel aliquet massa condimentum. Etiam at dapibus orci. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quam metus, lobortis sed volutpat id, elementum sed nibh. Nullam lacinia augue et pretium congue. Ut eleifend hendrerit ultricies. Ut hendrerit tellus porttitor purus convallis fermentum. Aenean cursus lacus libero, non maximus odio mattis quis. In a lorem tincidunt, lobortis ipsum sed, mattis sem. Morbi sapien dolor, sollicitudin eget libero ut, feugiat auctor tortor.</p>
+        </section>
+        <section>
+        <h3>Lorem</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce aliquam leo nec sem consectetur, vel aliquet massa condimentum. Etiam at dapibus orci. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quam metus, lobortis sed volutpat id, elementum sed nibh. Nullam lacinia augue et pretium congue. Ut eleifend hendrerit ultricies. Ut hendrerit tellus porttitor purus convallis fermentum. Aenean cursus lacus libero, non maximus odio mattis quis. In a lorem tincidunt, lobortis ipsum sed, mattis sem. Morbi sapien dolor, sollicitudin eget libero ut, feugiat auctor tortor.</p>
+        </section>
       </div>
     `;
   }
